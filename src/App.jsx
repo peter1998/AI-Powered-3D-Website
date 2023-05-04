4cb729572216659f60eec497e372db53dd2eb96d
Peter Matov <pmatov@gmail.com>
2023-05-04 15:54:53 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 className="head-text"> React App</h1>
+      <h1 className="head-text"> React App</h1>
+    </div>
+  );
+}
+
+export default App;
